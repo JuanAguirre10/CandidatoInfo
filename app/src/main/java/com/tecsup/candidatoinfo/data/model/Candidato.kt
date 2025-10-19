619ba6a78405d19c6a5ffcce1b858edcf6b1e0c0
JuanAguirre10 <juancho71096@gmail.com>
2025-10-18 20:46:59 -0500

    model candidato

--- app/src/main/java/com/tecsup/candidatoinfo/data/model/Candidato.kt
+++ app/src/main/java/com/tecsup/candidatoinfo/data/model/Candidato.kt
@@ -1,2 +1,14 @@
 package com.tecsup.candidatoinfo.data.model
 
+data class Candidato(
+    val id: String,
+    val nombreCompleto: String,
+    val partidoPolitico: String,
+    val cargo: String, // "Presidente" o "Congresista"
+    val fotoUrl: String,
+    val edad: Int,
+    val lugarNacimiento: String,
+    val profesion: String,
+    val numeroDenuncias: Int = 0,
+    val numeroProyectos: Int = 0
+)
