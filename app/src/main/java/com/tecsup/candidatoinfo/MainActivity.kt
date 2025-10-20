aaacb766ba2637e045e24dd96d624a95c76750fc
Samir Haziel <samir.alfonso@tecsup.edu.pe>
2025-10-20 08:02:09 -0500

    fix(ui): ajustar MainActivity con estructura de CandidatoInfoAppUI y NavigationHost

--- app/src/main/java/com/tecsup/candidatoinfo/MainActivity.kt
+++ app/src/main/java/com/tecsup/candidatoinfo/MainActivity.kt
@@ -3,16 +3,27 @@ package com.tecsup.candidatoinfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.tecsup.candidatoinfo.core.navigation.NavigationHost
-import com.tecsup.candidatoinfo.presentation.ui.theme.CandidatoInfoTheme
+import com.tecsup.candidatoinfo.ui.theme.CandidatoInfoTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CandidatoInfoTheme {
-                NavigationHost()
-            }
+            CandidatoInfoAppUI()
+        }
+    }
+}
+
+@Composable
+fun CandidatoInfoAppUI() {
+    CandidatoInfoTheme {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            NavigationHost()
         }
     }
 }
