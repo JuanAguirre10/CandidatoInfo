721961259799fbc4298a831c80c412ab36664e97
Samir Haziel <samir.alfonso@tecsup.edu.pe>
2025-10-20 00:30:28 -0500

    ui(theme): paleta cívica (azul/rojo/verde + neutros)

--- app/src/main/java/com/tecsup/candidatoinfo/ui/theme/Color.kt
+++ app/src/main/java/com/tecsup/candidatoinfo/ui/theme/Color.kt
@@ -2,10 +2,35 @@ package com.tecsup.candidatoinfo.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Colores principales de identidad cívica
+val Blue600 = Color(0xFF1976D2)  // Azul institucional
+val Blue400 = Color(0xFF42A5F5)
+val Blue50  = Color(0xFFE3F2FD)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Red600  = Color(0xFFD32F2F)  // Rojo para alertas/denuncias
+val Red400  = Color(0xFFEF5350)
+val Red50   = Color(0xFFFFEBEE)
+
+val Green600 = Color(0xFF388E3C) // Verde para proyectos
+val Green400 = Color(0xFF66BB6A)
+val Green50  = Color(0xFFE8F5E9)
+
+val Yellow600 = Color(0xFFFDD835) // Avisos
+val Yellow50  = Color(0xFFFFFDE7)
+
+// Neutros
+val Gray900 = Color(0xFF212121)
+val Gray700 = Color(0xFF616161)
+val Gray500 = Color(0xFF9E9E9E)
+val Gray300 = Color(0xFFE0E0E0)
+val Gray100 = Color(0xFFF5F5F5)
+val Gray50  = Color(0xFFFAFAFA)
+
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+
+// Semánticos
+val BackgroundLight = White
+val SurfaceLight    = Gray50
+val TextPrimary     = Gray900
+val TextSecondary   = Gray700
